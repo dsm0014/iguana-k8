bff61dd7398a0e7726687555c4a62735bbc38492
Daniel Smith Morrison <dsm0014@tigermail.auburn.edu>
2021-03-17 14:03:20 -0500

    load data on startup when necessary

--- src/main/java/com/dsm/iguana/api/IguanaApplication.java
+++ src/main/java/com/dsm/iguana/api/IguanaApplication.java
@@ -1,14 +1,21 @@
 package com.dsm.iguana.api;
 
+import com.dsm.iguana.api.service.IguanaService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IguanaApplication {
+public class IguanaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IguanaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		IguanaService.loadIguanaData();
 	}
 
 }
