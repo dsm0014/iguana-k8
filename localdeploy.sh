76ceea6d90bdb7fe00f144d87cb9604b228629c4
Daniel Smith Morrison <dsm0014@tigermail.auburn.edu>
2021-03-17 11:04:27 -0500

    support for Helm

--- localdeploy.sh
+++ localdeploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 namespace=$1
+[[ $2 = "upgrade" ]] && is_upgrade=true || is_upgrade=false
 
-kubectl apply -f kube/ -n $namespace
+if [ $is_upgrade ]; then
+  helm upgrade $namespace kube/simple-spring-mongo -n $namespace
+else
+  helm install $namespace kube/simple-spring-mongo -n $namespace
+fi
 minikube service spring-iguana --url -n $namespace
