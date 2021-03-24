9e8aaef0e461049c1fe7775fba597b37e2cea090
Daniel Smith Morrison <dsm0014@tigermail.auburn.edu>
2021-03-24 15:39:06 -0500

    add in memory mongodb

--- src/test/java/com/dsm/iguana/IguanaApplicationTests.java
+++ src/test/java/com/dsm/iguana/IguanaApplicationTests.java
@@ -1,7 +1,6 @@
 package com.dsm.iguana;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
