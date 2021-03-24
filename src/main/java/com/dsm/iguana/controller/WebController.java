2bd8d70e7ce511cb04d705d7dffc5648e1306acd
Daniel Smith Morrison <dsm0014@tigermail.auburn.edu>
2021-03-24 14:37:24 -0500

    iguana search functionality

--- src/main/java/com/dsm/iguana/controller/WebController.java
+++ src/main/java/com/dsm/iguana/controller/WebController.java
@@ -1,9 +1,12 @@
 package com.dsm.iguana.controller;
 
+import com.dsm.iguana.model.IguanaForm;
 import com.dsm.iguana.service.IguanaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,9 +15,16 @@ public class WebController {
     @Autowired
     private IguanaService iguanaService;
 
-    @RequestMapping({"/", "index"})
+    @RequestMapping({"/", "/home"})
     public String index(Model model) {
+        model.addAttribute("iguanaForm", new IguanaForm());
         model.addAttribute("iguanas", iguanaService.findAll());
+        return "index";
+    }
+
+    @RequestMapping(value="/search")
+    public String search(@ModelAttribute("iguanaForm") IguanaForm iguanaForm, BindingResult result, Model model) {
+        model.addAttribute("iguanas", iguanaService.search(iguanaForm.getUserInput()));
         return "index";
     }
 }
--- src/main/java/com/dsm/iguana/service/IguanaService.java
+++ src/main/java/com/dsm/iguana/service/IguanaService.java
@@ -6,16 +6,23 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
+import org.yaml.snakeyaml.tokens.ValueToken;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IguanaService {
 
 
     private static IguanaRepository repo;
+
+    private static List<Iguana> data = new ArrayList<>();
 
     @Autowired
     public IguanaService(IguanaRepository repo) {
@@ -43,6 +50,22 @@ public class IguanaService {
     }
 
     public List<Iguana> findAll() {
-        return repo.findAll();
+        if(data.size() < 1) {
+            data = repo.findAll();
+        }
+        return data;
+    }
+
+    public List<Iguana> search(String iguanaInput) {
+        if (StringUtils.isEmptyOrWhitespace(iguanaInput)) {
+            return this.findAll();
+        }
+        String iguana = iguanaInput.toLowerCase().trim();
+        return data.stream()
+                .filter(ig -> ig.getName().toLowerCase().contains(iguana)
+                           || ig.getBinomialName().toLowerCase().contains(iguana)
+                           || Arrays.stream(ig.getAlternateNames())
+                                .anyMatch(altName -> altName.toLowerCase().contains(iguana)))
+                .collect(Collectors.toList());
     }
 }
