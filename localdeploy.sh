ff355c4e879c88b0a3fc8c0453230e6ba5e6dd82
Daniel Smith Morrison <dsm0014@tigermail.auburn.edu>
2021-03-26 10:08:27 -0500

    move helm inside tf dir

--- localdeploy.sh
+++ localdeploy.sh
@@ -2,8 +2,8 @@
 namespace=$1
 deployment=$(helm list -q -n $namespace)
 if [ $deployment = $namespace ]; then
-  helm upgrade $namespace kube/simple-spring-mongo -n $namespace
+  helm upgrade $namespace tf/helm/simple-spring-mongo -n $namespace
 else
-  helm install $namespace kube/simple-spring-mongo -n $namespace
+  helm install $namespace tf/helm/simple-spring-mongo -n $namespace
 fi
 minikube service spring-iguana --url -n $namespace
